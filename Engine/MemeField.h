284001fa273aeb6b0abca9b938f98f0b887609a9
Danny Scarborough <danss@btinternet.com>
2021-03-16 00:37:24 +0000

    most game logic implemented

--- Engine/MemeField.h
+++ Engine/MemeField.h
@@ -11,26 +11,37 @@ private:
 		{
 			Hidden,
 			Flagged,
-			Reveiled
+			Revealed
 		};
 	public:
 		void SpawnMeme();
 		bool HasMeme() const;
-		void Draw(const Vei2& screenPos, Graphics& gfx) const;
+		void Draw(const Vei2& screenPos, bool fucked, Graphics& gfx) const;
+		void Reveal();
+		bool IsRevealed() const;
+		void ToggleFlag();
+		bool IsFlagged() const;
+		void SetNeighbourMemeCount(int memeCount);
 	private:
 		State state = State::Hidden;
 		bool hasMeme = false;
+		int nNeighbourMemes = -1;
 	};
 
 public:
 	MemeField(int nMemes);
 	void Draw(Graphics& gfx) const;
 	RectI GetRect() const;
+	void OnRevealClick(const Vei2& screenPos);
+	void OnFlagClick(const Vei2& screenPos);
 private:
 	Tile& TileAt(const Vei2& gridPos);
 	const Tile& TileAt(const Vei2& gridPos) const;
+	Vei2 ScreenToGrid(const Vei2& screenPos);
+	int CountNeighbourMemes(const Vei2& gridPos);
 private:
 	static constexpr int width = 20;
 	static constexpr int height = 16;
+	bool isFucked = false;
 	Tile field[width * height];
 };
